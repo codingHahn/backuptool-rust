666a8083545726505a9877c6884fe3301e1510b1
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-09 22:30:36 +0200

    fix module declaration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,6 @@
-mod configuration {
-    struct conf_struct {
-        exclude_patterns: Vec<String>,
-        source: String,
-        destination: String,
-        help: bool,
-    }
+struct conf_struct {
+    exclude_patterns: Vec<String>,
+    source: String,
+    destination: String,
+    help: bool,
 }
