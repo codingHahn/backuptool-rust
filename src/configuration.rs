0f882fe975ddb023cc8ad830360b7628a863d69d
Nick Hahn <nick.hahn@posteo.de>
2021-04-12 14:14:12 +0200

    Change src and dest fields of config struct to PathBuf
    Before, they were simple strings. Rust offers a discrete Path type for
    this kind of thing. PathBuf it's what String is to &str.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,8 @@
+use std::path::PathBuf;
+
 pub struct ConfStruct {
     pub exclude_patterns: Vec<String>,
-    pub source: String,
-    pub destination: String,
+    pub source: PathBuf,
+    pub destination: PathBuf,
     pub help: bool,
 }
