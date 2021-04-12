27029e15d126f3082e2bb40856e72f9070d02c7c
Nick Hahn <nick.hahn@posteo.de>
2021-04-12 15:56:29 +0200

    Rename field exclude_clearname to _string for clarity

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,7 +2,7 @@ use regex::RegexSet;
 use std::path::PathBuf;
 
 pub struct ConfStruct {
-    pub exclude_clearname: Vec<String>,
+    pub exclude_strings: Vec<String>,
     pub exclude_regex: RegexSet,
     pub source: PathBuf,
     pub destination: PathBuf,
