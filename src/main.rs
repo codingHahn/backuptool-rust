6acabbafdce017e3c28b2301842df1a6c9bd9e33
Nick Hahn <nick.hahn@posteo.de>
2021-04-12 15:50:37 +0200

    Initial string-based exclude mechanism
    The implementation takes every path in a given path and checks if it is
    mentioned in the exclude vector. If that isn't the case, push it onto a
    new vector which only contains non-excluded paths. At the end, print all
    non-excluded files.
    
    INFO: Waites on the parser implementation and the split between
    `exclude_strings` and `exclude_regex`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
 mod cli_parser;
 mod configuration;
+use std::fs;
+use std::path::PathBuf;
 
 #[cfg(test)]
 mod cli_tests;
 
 fn main() {
-    println!("Hello, world!");
+    // A example config to test the implementation
+    let config = configuration::ConfStruct {
+        exclude_patterns : vec![".git".to_string()],
+        source : "./".to_string(),
+        destination : "./dest".to_string(),
+        help : false,
+    };
+    let files_in_source = fs::read_dir(&config.source).unwrap();
+
+    println!("Files in source folder: {}", config.source);
+
+    let mut path_wo_excludes : Vec<PathBuf> = Vec::new();
+
+    // Check if any paths are in the exclude_patterns and remove them if they are
+    // TODO: Handle when destination is in source
+    for path_result in files_in_source {
+        match path_result {
+            Err(why) => panic!("Something bad happened: {}", why),
+            Ok(path) => {
+                let p = path.file_name().into_string().unwrap();
+                if !config.exclude_patterns.contains(&p) {
+                    path_wo_excludes.push(path.path());
+                }
+            },
+        };
+    }
+
+    println!("Final file list without excludes: {:?}", path_wo_excludes);
 }
