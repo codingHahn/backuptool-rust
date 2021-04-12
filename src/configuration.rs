64f16097d790807edbe36979736283e296baee0c
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-12 15:41:33 +0200

    add RegexSet to ConfStruct

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,9 @@
+use regex::RegexSet;
 use std::path::PathBuf;
 
 pub struct ConfStruct {
-    pub exclude_patterns: Vec<String>,
+    pub exclude_clearname: Vec<String>,
+    pub exclude_regex: RegexSet,
     pub source: PathBuf,
     pub destination: PathBuf,
     pub help: bool,
