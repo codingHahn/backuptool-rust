919617376eabd6718dc0ae587238e7b2502bbfdc
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-12 17:29:24 +0200

    implement CLIParser for new ConfSrtuct

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -1,10 +1,14 @@
 use crate::configuration;
 use std::env;
 use std::result::Result;
+use std::path::PathBuf;
+
+use regex::RegexSet;
 
 enum CLIOptions {
-    SourceDestination { source: String, destination: String },
-    ExcludePattern { pattern: String },
+    SourceDestination { source: PathBuf, destination: PathBuf },
+    ExcludeString { string: String },
+    ExcludeRegex { regex: String },
     Help,
     End,
 }
@@ -14,6 +18,7 @@ fn get_option_type(args: &Vec<String>, index: usize) -> (usize, Result<CLIOption
         // Exclude pattern in next element
         Option::None => return (index, Result::Ok(CLIOptions::End)),
         Option::Some(arg0) => match arg0.as_str().trim() {
+            // Exclude String:
             "-e" => match args.get(index + 1) {
                 Option::None => {
                     return (
@@ -24,12 +29,29 @@ fn get_option_type(args: &Vec<String>, index: usize) -> (usize, Result<CLIOption
                 Option::Some(arg1) => {
                     return (
                         index + 2,
-                        Result::Ok(CLIOptions::ExcludePattern {
-                            pattern: String::from(arg1),
+                        Result::Ok(CLIOptions::ExcludeString {
+                            string: String::from(arg1),
                         }),
                     )
                 }
             },
+            //Exclude regex
+            "-er" => match args.get(index + 1) {
+                Option::None => {
+                    return (
+                        index,
+                        Result::Err(String::from("missing Argument after -er")),
+                    )
+                }
+                Option::Some(arg1) => {
+                    return (
+                        index + 2,
+                        Result::Ok(CLIOptions::ExcludeRegex {
+                            regex: String::from(arg1),
+                        }),
+                    )
+                }
+            }
             // help tag
             "-?" => return (index + 1, Result::Ok(CLIOptions::Help)),
             // other: source + destination
@@ -39,8 +61,8 @@ fn get_option_type(args: &Vec<String>, index: usize) -> (usize, Result<CLIOption
                     return (
                         index + 2,
                         Result::Ok(CLIOptions::SourceDestination {
-                            source: String::from(arg0),
-                            destination: String::from(arg1),
+                            source: PathBuf::from(arg0),
+                            destination: PathBuf::from(arg1),
                         }),
                     )
                 }
@@ -55,20 +77,21 @@ fn print_error(index: &usize, msg: String) {
 }
 
 fn print_help_message() {
-    println!("usage: backup-tool <-e EXCLUDE_PATTERNS> [Source] [Destination]");
-    println!("-e: Pattern to exclude (can be given more than once)");
+    println!("usage: backup-tool <-e EXCLUDE_PATTERNS> <-er Exclude_REGEX> [Source] [Destination]");
+    println!("-e: Folder or File to exclude (can be given more than once)");
+    println!("-er: Regular expression to exclude (can be given more than once)");
     println!("-?: help");
     println!("Source: the source for the backup");
     println!("Destination: the destination for the backup");
 }
 
 pub fn parse_cli_options() -> configuration::ConfStruct {
-    let mut conf_struct = configuration::ConfStruct {
-        exclude_patterns: Vec::new(),
-        source: String::new(),
-        destination: String::new(),
-        help: false,
-    };
+    // first collect all fields of ConfStruct seperately
+    let mut conf_exclude_strings: Vec<String> = Vec::new();
+    let mut conf_exclude_regex: Vec<String> = Vec::new();
+    let mut conf_source: PathBuf = PathBuf::new();
+    let mut conf_dest : PathBuf = PathBuf::new();
+    let mut conf_help: bool = false;
 
     let args: Vec<String> = env::args().collect();
     // transform all args into conf_struct content
@@ -90,22 +113,28 @@ pub fn parse_cli_options() -> configuration::ConfStruct {
                     source,
                     destination,
                 } => {
-                    if !conf_struct.destination.is_empty() {
+                    if !conf_dest.as_os_str().is_empty() {
                         print_error(&index, String::from("destination given to often"));
                         error = true;
-                    } else if !conf_struct.source.is_empty() {
+                    } else if !conf_source.as_os_str().is_empty() {
                         print_error(&index, String::from("source given to often"));
                         error = true;
                     } else {
-                        conf_struct.destination = destination;
-                        conf_struct.source = source;
+                        conf_dest = destination;
+                        conf_source = source;
                     }
                 }
-                CLIOptions::ExcludePattern { pattern } => {
-                    conf_struct.exclude_patterns.push(pattern)
-                }
+                CLIOptions::ExcludeString { string } => {
+                    conf_exclude_strings.push(string);
+                },
+                CLIOptions::ExcludeRegex { regex } => {
+                    conf_exclude_regex.push(regex);
+                },
                 CLIOptions::End => finished = true,
-                CLIOptions::Help => print_help_message(),
+                CLIOptions::Help => {
+                    print_help_message();
+                    conf_help = true;
+                }
             },
         }
 
@@ -116,6 +145,13 @@ pub fn parse_cli_options() -> configuration::ConfStruct {
             break;
         }
     }
-
-    conf_struct
+    
+    //return ConfStruct
+    configuration::ConfStruct {
+        exclude_strings: conf_exclude_strings,
+        exclude_regex: RegexSet::new(conf_exclude_regex).unwrap(),
+        destination: conf_dest,
+        source: conf_source,
+        help: conf_help,
+    }
 }
