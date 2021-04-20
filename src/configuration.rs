49625dd1ea972fae4ee2acbb045f7b830d8ac993
Nick Hahn <nick.hahn@posteo.de>
2021-04-20 23:06:59 +0200

    Implement new function for ConfStruct
    For idiomatic reasons, a reasonably advanced struct should have a
    constructor.

--- src/configuration.rs
+++ src/configuration.rs
@@ -8,3 +8,9 @@ pub struct ConfStruct {
     pub destination: PathBuf,
     pub verbose: bool,
 }
+
+impl ConfStruct {
+    pub fn new(exclude_strings: Vec<String>, exclude_regex: RegexSet, source: PathBuf, destination: PathBuf, verbose: bool) -> Self {
+        ConfStruct{ exclude_strings: exclude_strings, exclude_regex: exclude_regex, source: source, destination: destination, verbose: verbose }
+    }
+}
